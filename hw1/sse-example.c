0d740842388c13ef3ac8120d652f6c06928bbbbb
Karthik Narayan <karthik@karthik.(none)>
2015-02-08 14:49:27 -0800

    Adding sse example.

---
+++ hw1/sse-example.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include <emmintrin.h>
+#include <pmmintrin.h>
+
+float reduce(const float* a, int n) {
+  float sum[4];
+  __m128 vsum = _mm_set1_ps(0);
+
+  // Ensure that n is a multiple of 4
+  assert((n & 3) == 0);
+
+  // Ensure that a is 16-byte aligned
+  assert(((uintptr_t) a & 15) == 0);
+
+  // Execute the adds
+  for (int i = 0; i < n; i += 4) {
+    __m128 value = _mm_load_ps(&a[i]);
+    vsum = _mm_add_ps(vsum, value);
+  }
+
+  // Store results back in sum
+  _mm_store_ps(sum, vsum);
+
+  // Return
+  return sum[0] + sum[1] + sum[2] + sum[3];
+}
+
+int main() {
+  int N = 20;
+  void* mem = malloc(sizeof(float) * N + 15);
+  float* ptr = (float*) (((uintptr_t) mem + 15) & ~(uintptr_t) 0x0F);
+
+  for (int i = 0; i < N; ++i) ptr[i] = i;
+
+  printf("sum: %f\n", reduce(ptr, N));
+  printf("sum: %f\n", N * (N + 1) / 2);
+
+  return 0;
+}
+
