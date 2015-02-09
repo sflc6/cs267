04b35798da89d93c3de1bab825d905284980cca4
Karthik Narayan <karthik@karthik.(none)>
2015-02-08 19:43:39 -0800

    Updated block size.

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -11,7 +11,7 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 2
+#define BLOCK_SIZE 32
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -51,12 +51,6 @@ inline double dotAVX(double* a, double* b, int n) {
 
 __attribute__((always_inline))
 inline double dotSSE(double* a, double* b, int n) {
-  // printf("align: %d\n", get_align32(a));
-  // printf("align: %d\n\n", get_align32(b));
-
-  // assert(get_align32(a) == 0);
-  // assert(get_align32(b) == 0);
-
   double dot[2];
   __m128d vdot = _mm_set1_pd(0);
 
